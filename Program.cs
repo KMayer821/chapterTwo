e1eb62d6ec39bcebbe75e6ddf14d8c8c81329b3c
KMayer821 <91973799+KMayer821@users.noreply.github.com>
2022-03-14 22:11:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int integer = 1;
+            double floatPoint = 3.22;
+            bool isMeAlife = true;
+            string Message = "some message";
+            float fifth = 1.337;
+            long bigDec = 111111111;
+
+        }
+    }
+}
